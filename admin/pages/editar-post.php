b70f53ba44f1ee7218d567311ab01aa6778c0dcc
Cauã <cauaserpa092@gmail.com>
2025-07-18 02:25:39 -0300

    [feat] adicionar botão "Visualizar" após criação do post no lugar do botão "Resetar"

--- admin/pages/editar-post.php
+++ admin/pages/editar-post.php
@@ -275,7 +275,16 @@
                     </div>
 
                     <div class="d-flex">
-                        <button type="button" class="btn btn-1" onclick="location.reload();">Resetar</button>
+                        <!-- Botão Visualizar -->
+                        <a href="<?php echo INCLUDE_PATH . "post?id={$post['id']}"; ?>" target="_blank" class="btn btn-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler-external-link">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+                                <path d="M11 13l9 -9" />
+                                <path d="M15 4h5v5" />
+                            </svg>
+                            Visualizar
+                        </a>
                         <button type="submit" name="btnSubmit" id="btnSubmit" class="btn btn-primary ms-auto">Salvar</button>
                     </div>
 
