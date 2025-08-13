e956aae86a1c401dd68fb90491300b3ccc566000
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-08-13 12:56:38 -0300

    fix: update htaccess

--- admin/template-parts/header.php
+++ admin/template-parts/header.php
@@ -64,7 +64,7 @@
         $url == 'mensagens' ||
         $url == 'usuarios' ||
         $url == 'vendedoras' ||
-        $url == 'rodape' ||
+        $url == 'contatos' ||
         $url == 'aparencia' ||
         $url == 'paginas' ||
         $url == 'politica-de-privacidade' ||
@@ -126,7 +126,6 @@
                             <li class="nav-item <?= activeSidebarLink('usuarios'); ?> <?= activeSidebarLink('criar-usuario'); ?> <?= activeSidebarLink('editar-usuario'); ?>">
                                 <a class="nav-link" href="<?php echo INCLUDE_PATH_ADMIN; ?>usuarios" >
                                     <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                        <!-- Download SVG icon from http://tabler.io/icons/icon/user -->
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1 icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
                                     </span>
                                     <span class="nav-link-title">
@@ -140,7 +139,6 @@
                             <li class="nav-item <?= activeSidebarLink('vendedoras'); ?> <?= activeSidebarLink('criar-usuario'); ?> <?= activeSidebarLink('editar-usuario'); ?>">
                                 <a class="nav-link" href="<?php echo INCLUDE_PATH_ADMIN; ?>vendedoras" >
                                     <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                        <!-- Download SVG icon from http://tabler.io/icons/icon/user -->
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1 icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
                                     </span>
                                     <span class="nav-link-title">
@@ -150,11 +148,11 @@
                             </li>
                             <?php endif; ?>
 
-                            <?php if (verificaPermissao($_SESSION['user_id'], 'rodape', 'read', $conn)): ?>
-                            <li class="nav-item <?= activeSidebarLink('rodape'); ?>">
-                                <a class="nav-link" href="<?php echo INCLUDE_PATH_ADMIN; ?>rodape" >
+                            <?php if (verificaPermissao($_SESSION['user_id'], 'contatos', 'read', $conn)): ?>
+                            <li class="nav-item <?= activeSidebarLink('contatos'); ?>">
+                                <a class="nav-link" href="<?php echo INCLUDE_PATH_ADMIN; ?>contatos" >
                                     <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1 icon-tabler icons-tabler-outline icon-tabler-messages"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" /><path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" /></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1 icon-tabler icons-tabler-outline icon-tabler-messages"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" /><path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" /></svg>
                                     </span>
                                     <span class="nav-link-title">
                                         Contatos
@@ -167,7 +165,6 @@
                             <li class="nav-item <?= activeSidebarLink('aparencia'); ?>">
                                 <a class="nav-link" href="<?php echo INCLUDE_PATH_ADMIN; ?>aparencia" >
                                     <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                        <!-- Download SVG icon from http://tabler.io/icons/icon/home -->
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1 icon-tabler icons-tabler-outline icon-tabler-paint"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" /><path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2" /><path d="M10 15m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /></svg>
                                     </span>
                                     <span class="nav-link-title">
