dc03484573038748971c452e03d90c868ccced04
Alexandre Tucunduva <motive.mobi@gmail.com>
2025-12-01 11:39:22 -0300

    [fix] correção na exibição do nome do admin no menu horizontal

--- admin/index.php
+++ admin/index.php
@@ -26,7 +26,7 @@ $id = 1;
 $user_id = $_SESSION['user_id'];
 
 // Consulta SQL
-$sql = "SELECT * FROM $tabela WHERE id = :id";
+$sql = "SELECT tk.*, tc.nome as nome_cliente FROM $tabela as tk JOIN $tabela_5 as tc WHERE tk.id = :id AND tc.id = 1";
 $sql_2 = "SELECT fb_pixel, gtm, g_analytics FROM $tabela_2 WHERE id = :id";
 $sql_3 = "SELECT privacy_policy, use_privacy FROM $tabela_3 WHERE id = :id";
 $sql_5 = "SELECT * FROM $tabela_5 WHERE roles != 1 AND id = :id";
@@ -78,12 +78,11 @@ $resultado_6 = $stmt_6->fetchAll(PDO::FETCH_ASSOC);
 $resultado_7 = $stmt_7->fetch(PDO::FETCH_ASSOC);
 $resultado_8 = $stmt_8->fetchAll(PDO::FETCH_ASSOC);
 
-
-
     // Verificar se o resultado foi encontrado
     if ($resultado) {
         // Atribuir o valor da coluna à variável, ex.: "nome" = $nome
         $nome = $resultado['nome'];
+        $cliente_nome = $resultado['nome_cliente'];
         $permissao = getNomePermissao($_SESSION['user_id'], $conn);
         $logo = $resultado['logo'];
         $title = $resultado['title'];
